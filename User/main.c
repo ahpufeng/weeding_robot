3c5ddbbee67e6b6c692590beb19af174558739a3
Eric Lai <qq584863350@gmail.com>
2016-03-08 01:01:38 +0800

    first commit

---
+++ User/main.c
@@ -0,0 +1,144 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2013-xx-xx
+  * @brief   火眼ov7725照相机实验
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 iSO STM32 开发板 
+  * 论坛    :http://www.chuxue123.com
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+	*/
+#include "stm32f10x.h"
+#include "stdio.h"
+#include "bsp_usart1.h"
+#include "bsp_usart2.h"
+#include "bsp_ili9341_lcd.h"
+#include "bsp_sdfs_app.h"
+#include "bsp_bmp.h"
+#include "bsp_ov7725.h"
+#include "bsp_exti.h"
+#include "bsp_led.h"
+#include "wifi_config.h"
+#include "wifi_function.h"
+#include "bsp_SysTick.h"
+#include <string.h>
+
+/*
+ * 全局中断优先级的高低：串口 > (按键) > sd卡 > 摄像头场中断   
+ */
+ 
+#define IP_ADDRESS "172.21.30.1";
+
+extern volatile uint8_t Ov7725_vsync ;
+extern char serverIpAddress[20];
+extern char *pIpAddress;
+volatile uint8_t screen_flag;
+char * pStr;
+
+void LcdAndSD_Init()
+{
+		/* 液晶初始化 */
+		LCD_Init();
+		
+		/* 设置液晶扫描方向为 左下角->右上角 */
+		Lcd_GramScan( 2 );
+		LCD_Clear(0, 0, 320, 240, BACKGROUND);
+
+		/* 初始化sd卡文件系统，因为汉字的字库和bmp图片放在了sd卡里面 */
+		Sd_fs_init();
+}
+
+void LCDshowStatus()
+{
+		LCD_Init();
+		Lcd_GramScan(1);
+		LCD_Clear(0, 0, 240, 320, BACKGROUND);	
+		LCD_DispStr(0,0,(uint8_t *)"finished screenshot",0xffff);
+}
+
+void showVideo(void)
+{
+    uint8_t file_name[20];
+    uint8_t num = 0;
+		uint8_t flag = 1;
+/*--------------------------------------------------------------------------------------------------------*/		
+	  /* 按键初始化 */
+		EXTI_PC13_Config();
+	
+		/* LED 初始化 */
+		LED_GPIO_Config();
+		LED1_OFF;
+		LED2_OFF;
+/*--------------------------------------------------------------------------------------------------------*/		
+		LcdAndSD_Init();
+/*--------------------------------------------------------------------------------------------------------*/	
+		/* ov7725 gpio 初始化 */
+		Ov7725_GPIO_Config();
+	
+		/* ov7725 寄存器配置初始化 */
+		while(Ov7725_Init() != SUCCESS);
+	
+		/* ov7725 场信号线初始化 */
+		VSYNC_Init();	
+		Ov7725_vsync = 0;
+/*-------------------------------------------------------------------------------------------------------*/	
+		while(flag)
+		{
+				if( Ov7725_vsync == 2 )
+						{
+								FIFO_PREPARE;  			/*FIFO准备*/					
+								ImagDisp();					/*采集并显示*/
+								Ov7725_vsync = 0;			
+						}        
+				/* screen_flag 在按键中断函数里面置位 */
+				if( screen_flag == 1 )
+				{                    
+						sprintf((char *)&file_name,"/camera%d",num++);           
+										
+						/* 设置液晶扫描方向为 右下角->左上角 */
+						Lcd_GramScan( 3 );
+						/* 截屏，并保存到SD卡上 */
+						Screen_shot(0,0,320,240,file_name);
+						
+						/* 截图完毕LED2灭 */
+						LED2_ON;
+						screen_flag = 0;
+						flag = 0;
+						VSYNC_NVIC_Disable();
+						LCDshowStatus();
+				}	
+		}
+}
+
+void WifiPrepare()
+{
+	pIpAddress = IP_ADDRESS;
+	strcpy ( serverIpAddress, ( const char * ) pIpAddress );
+	WiFi_Config();
+	SysTick_Init();
+}
+
+int main()
+{
+
+	USART1_Config();
+//	showVideo();
+	WifiPrepare();
+	ESP8266_STA_TCP_Client();
+//	pStr = ESP8266_ReceiveString ( ENABLE );
+//	PC_Usart("%s", pStr);
+//	LCD_Clear(0, 0, 240, 320, BACKGROUND);	
+//	LCD_DispStr(0, 220, (uint8_t *) pStr, 0xffff);
+
+}
+/* ---------------------------------------------end of file----------------------------------------------*/
+
+
+	  
+
---
+++ User/wifi/wifi_function.c
@@ -0,0 +1,501 @@
+#include "wifi_function.h"
+#include "wifi_config.h"
+//#include "bsp_gpio.h"
+#include "bsp_usart1.h"
+#include "bsp_usart2.h"
+#include "bsp_SysTick.h"
+#include <string.h> 
+#include <stdio.h>  
+#include <stdbool.h>
+#include "bsp_ili9341_lcd.h"
+
+char serverIpAddress[20];
+char *pIpAddress;
+
+/*
+ * 函数名：ESP8266_Choose
+ * 描述  ：使能/禁用WF-ESP8266模块
+ * 输入  ：enumChoose = ENABLE，使能模块
+ *         enumChoose = DISABLE，禁用模块
+ * 返回  : 无
+ * 调用  ：被外部调用
+ */
+void ESP8266_Choose ( FunctionalState enumChoose )
+{
+	if ( enumChoose == ENABLE )
+		ESP8266_CH_HIGH_LEVEL();
+	
+	else
+		ESP8266_CH_LOW_LEVEL();
+}
+
+/*
+ * 函数名：ESP8266_Rst
+ * 描述  ：重启WF-ESP8266模块
+ * 输入  ：无
+ * 返回  : 无
+ * 调用  ：被ESP8266_AT_Test调用
+ */
+void ESP8266_Rst ( void )
+{
+	#if 0
+	 ESP8266_Cmd ( "AT+RST", "OK", "ready", 2500 );   	
+	
+	#else
+	 ESP8266_RST_LOW_LEVEL();
+	 Delay_ms ( 500 ); 
+	 ESP8266_RST_HIGH_LEVEL();
+	 
+	#endif
+}
+
+/*
+ * 函数名：ESP8266_AT_Test
+ * 描述  ：对WF-ESP8266模块进行AT测试启动
+ * 输入  ：无
+ * 返回  : 无
+ * 调用  ：被外部调用
+ */
+void ESP8266_AT_Test ( void )
+{
+	ESP8266_RST_HIGH_LEVEL();
+	
+	Delay_ms ( 1000 ); 
+	
+	while ( ! ESP8266_Cmd ( "AT", "OK", NULL, 200 ) ) ESP8266_Rst ();  	
+
+}
+
+
+/*
+ * 函数名：ESP8266_Cmd
+ * 描述  ：对WF-ESP8266模块发送AT指令
+ * 输入  ：cmd，待发送的指令
+ *         reply1，reply2，期待的响应，为NULL表不需响应，两者为或逻辑关系
+ *         waittime，等待响应的时间
+ * 返回  : 1，指令发送成功
+ *         0，指令发送失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Cmd ( char * cmd, char * reply1, char * reply2, u32 waittime )
+{    
+	strEsp8266_Fram_Record .InfBit .FramLength = 0;               //从新开始接收新的数据包
+
+	ESP8266_Usart ( "%s\r\n", cmd );
+
+	if ( ( reply1 == 0 ) && ( reply2 == 0 ) )                      //不需要接收数据
+		return true;
+	
+	Delay_ms ( waittime );                 //延时
+	
+	strEsp8266_Fram_Record .Data_RX_BUF [ strEsp8266_Fram_Record .InfBit .FramLength ]  = '\0';
+
+	PC_Usart ( "%s", strEsp8266_Fram_Record .Data_RX_BUF );
+  
+	if ( ( reply1 != 0 ) && ( reply2 != 0 ) )
+		return ( ( bool ) strstr ( strEsp8266_Fram_Record .Data_RX_BUF, reply1 ) || 
+						 ( bool ) strstr ( strEsp8266_Fram_Record .Data_RX_BUF, reply2 ) ); 
+ 	
+	else if ( reply1 != 0 )
+		return ( ( bool ) strstr ( strEsp8266_Fram_Record .Data_RX_BUF, reply1 ) );
+	
+	else
+		return ( ( bool ) strstr ( strEsp8266_Fram_Record .Data_RX_BUF, reply2 ) );
+	
+}
+
+
+/*
+ * 函数名：ESP8266_Net_Mode_Choose
+ * 描述  ：选择WF-ESP8266模块的工作模式
+ * 输入  ：enumMode，工作模式
+ * 返回  : 1，选择成功
+ *         0，选择失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Net_Mode_Choose ( ENUM_Net_ModeTypeDef enumMode )
+{
+	switch ( enumMode )
+	{
+		case STA:
+			return ESP8266_Cmd ( "AT+CWMODE=1", "OK", "no change", 2500 ); 
+		
+	  case AP:
+		  return ESP8266_Cmd ( "AT+CWMODE=2", "OK", "no change", 2500 ); 
+		
+		case STA_AP:
+		  return ESP8266_Cmd ( "AT+CWMODE=3", "OK", "no change", 2500 ); 
+		
+	  default:
+		  return false;
+  }
+	
+}
+
+
+/*
+ * 函数名：ESP8266_JoinAP
+ * 描述  ：WF-ESP8266模块连接外部WiFi
+ * 输入  ：pSSID，WiFi名称字符串
+ *       ：pPassWord，WiFi密码字符串
+ * 返回  : 1，连接成功
+ *         0，连接失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_JoinAP ( char * pSSID, char * pPassWord )
+{
+	char cCmd [120];
+
+	sprintf ( cCmd, "AT+CWJAP=\"%s\",\"%s\"", pSSID, pPassWord );
+	
+	return ESP8266_Cmd ( cCmd, "OK", NULL, 7000 );
+	
+}
+
+
+/*
+ * 函数名：ESP8266_BuildAP
+ * 描述  ：WF-ESP8266模块创建WiFi热点
+ * 输入  ：pSSID，WiFi名称字符串
+ *       ：pPassWord，WiFi密码字符串
+ *       ：enunPsdMode，WiFi加密方式代号字符串
+ * 返回  : 1，创建成功
+ *         0，创建失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_BuildAP ( char * pSSID, char * pPassWord, char * enunPsdMode )
+{
+	char cCmd [120];
+
+	sprintf ( cCmd, "AT+CWSAP=\"%s\",\"%s\",1,%s", pSSID, pPassWord, enunPsdMode );
+	
+	return ESP8266_Cmd ( cCmd, "OK", 0, 1000 );
+	
+}
+
+
+/*
+ * 函数名：ESP8266_Enable_MultipleId
+ * 描述  ：WF-ESP8266模块启动多连接
+ * 输入  ：enumEnUnvarnishTx，配置是否多连接
+ * 返回  : 1，配置成功
+ *         0，配置失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Enable_MultipleId ( FunctionalState enumEnUnvarnishTx )
+{
+	char cStr [20];
+	
+	sprintf ( cStr, "AT+CIPMUX=%d", ( enumEnUnvarnishTx ? 1 : 0 ) );
+	
+	return ESP8266_Cmd ( cStr, "OK", 0, 500 );
+	
+}
+
+/*
+ * 函数名：ESP8266_Tcp_Close
+ * 描述  ：WF-ESP8266模块启动多连接
+ * 输入  ：enumEnUnvarnishTx，配置是否多连接
+ * 返回  : 1，配置成功
+ *         0，配置失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Tcp_Close (void)
+{
+	char cStr [20];
+	
+	sprintf ( cStr, "AT+CIPCLOSE");
+	
+	return ESP8266_Cmd ( cStr, "OK", 0, 500 );
+	
+}
+
+/*
+ * 函数名：ESP8266_Link_Server
+ * 描述  ：WF-ESP8266模块连接外部服务器
+ * 输入  ：enumE，网络协议
+ *       ：ip，服务器IP字符串
+ *       ：ComNum，服务器端口字符串
+ *       ：id，模块连接服务器的ID
+ * 返回  : 1，连接成功
+ *         0，连接失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Link_Server ( ENUM_NetPro_TypeDef enumE, char * ip, char * ComNum, ENUM_ID_NO_TypeDef id)
+{
+	char cStr [100] = { 0 }, cCmd [120];
+
+  switch (  enumE )
+  {
+		case enumTCP:
+		  sprintf ( cStr, "\"%s\",\"%s\",%s", "TCP", ip, ComNum );
+		  break;
+		
+		case enumUDP:
+		  sprintf ( cStr, "\"%s\",\"%s\",%s", "UDP", ip, ComNum );
+		  break;
+		
+		default:
+			break;
+  }
+
+  if ( id < 5 )
+    sprintf ( cCmd, "AT+CIPSTART=%d,%s", id, cStr);
+
+  else
+	  sprintf ( cCmd, "AT+CIPSTART=%s", cStr );
+
+	return ESP8266_Cmd ( cCmd, "OK", "ALREAY CONNECT", 500 );
+	
+}
+
+
+/*
+ * 函数名：ESP8266_StartOrShutServer
+ * 描述  ：WF-ESP8266模块开启或关闭服务器模式
+ * 输入  ：enumMode，开启/关闭
+ *       ：pPortNum，服务器端口号字符串
+ *       ：pTimeOver，服务器超时时间字符串，单位：秒
+ * 返回  : 1，操作成功
+ *         0，操作失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_StartOrShutServer ( FunctionalState enumMode, char * pPortNum, char * pTimeOver )
+{
+	char cCmd1 [120], cCmd2 [120];
+
+	if ( enumMode )
+	{
+		sprintf ( cCmd1, "AT+CIPSERVER=%d,%s", 1, pPortNum );
+		
+		sprintf ( cCmd2, "AT+CIPSTO=%s", pTimeOver );
+
+		return ( ESP8266_Cmd ( cCmd1, "OK", 0, 500 ) &&
+						 ESP8266_Cmd ( cCmd2, "OK", 0, 500 ) );
+	}
+	
+	else
+	{
+		sprintf ( cCmd1, "AT+CIPSERVER=%d,%s", 0, pPortNum );
+
+		return ESP8266_Cmd ( cCmd1, "OK", 0, 500 );
+	}
+	
+}
+
+/*
+ * 函数名：ESP8266_UnvarnishSend
+ * 描述  ：配置WF-ESP8266模块进入透传发送
+ * 输入  ：无
+ * 返回  : 1，配置成功
+ *         0，配置失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_UnvarnishSend ( void )
+{
+	return (
+	  ESP8266_Cmd ( "AT+CIPMODE=1", "OK", 0, 500 ) &&
+	  ESP8266_Cmd ( "AT+CIPSEND", "\r\n", ">", 500 ) );
+	
+}
+
+/*
+ * 函数名：ESP8266_Disable_UnvarnishSend
+ * 描述  ：配置WF-ESP8266模块退出透传发送
+ * 输入  ：无
+ * 返回  : 1，配置成功
+ *         0，配置失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Disable_UnvarnishSend ( void )
+{
+	return ESP8266_Cmd ( "AT+CIPMODE=0", "OK", 0, 500 );
+}
+
+
+/*
+ * 函数名：ESP8266_Return_At
+ * 描述  ：配置WF-ESP8266模块退出透传发送
+ * 输入  ：无
+ * 返回  : 1，配置成功
+ *         0，配置失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_Return_At ( void )
+{
+	USART_ClearFlag(USART2, USART_FLAG_TC);
+	Delay_ms(1000);	
+	while((USART2->SR&0X40)==0);	
+	USART2->DR='+';      
+	Delay_ms(15);					
+	while((USART2->SR&0X40)==0);	
+	USART2->DR='+';      
+	Delay_ms(15);					
+	while((USART2->SR&0X40)==0);	
+	USART2->DR='+';      
+	Delay_ms(1000);					
+	return ESP8266_Cmd("AT","OK",0,20);
+	
+}
+
+
+/*
+ * 函数名：ESP8266_SendString
+ * 描述  ：WF-ESP8266模块发送字符串
+ * 输入  ：enumEnUnvarnishTx，声明是否已使能了透传模式
+ *       ：pStr，要发送的字符串
+ *       ：ulStrLength，要发送的字符串的字节数
+ *       ：ucId，哪个ID发送的字符串
+ * 返回  : 1，发送成功
+ *         0，发送失败
+ * 调用  ：被外部调用
+ */
+bool ESP8266_SendString ( FunctionalState enumEnUnvarnishTx, char * pStr, u32 ulStrLength, ENUM_ID_NO_TypeDef ucId )
+{
+	char cStr [20];
+	bool bRet = false;
+		
+	if ( enumEnUnvarnishTx )
+		ESP8266_Usart ( "%s", pStr );
+
+	
+	else
+	{
+		if ( ucId < 5 )
+			sprintf ( cStr, "AT+CIPSEND=%d,%d", ucId, ulStrLength + 2 );
+
+		else
+			sprintf ( cStr, "AT+CIPSEND=%d", ulStrLength + 2 );
+		
+		ESP8266_Cmd ( cStr, "> ", 0, 1000 );
+
+		bRet = ESP8266_Cmd ( pStr, "SEND OK", 0, 1000 );
+  }
+	
+	return bRet;
+
+}
+
+
+/*
+ * 函数名：ESP8266_ReceiveString
+ * 描述  ：WF-ESP8266模块接收字符串
+ * 输入  ：enumEnUnvarnishTx，声明是否已使能了透传模式
+ * 返回  : 接收到的字符串首地址
+ * 调用  ：被外部调用
+ */
+char * ESP8266_ReceiveString ( FunctionalState enumEnUnvarnishTx )
+{
+	char * pRecStr = 0;
+	
+	strEsp8266_Fram_Record .InfBit .FramLength = 0;
+	strEsp8266_Fram_Record .InfBit .FramFinishFlag = 0;
+	while ( ! strEsp8266_Fram_Record .InfBit .FramFinishFlag );
+	strEsp8266_Fram_Record .Data_RX_BUF [ strEsp8266_Fram_Record .InfBit .FramLength ] = '\0';
+	
+	if ( enumEnUnvarnishTx )
+	{
+		if ( strstr ( strEsp8266_Fram_Record .Data_RX_BUF, ">" ) )
+			pRecStr = strEsp8266_Fram_Record .Data_RX_BUF;
+
+	}
+	
+	else 
+	{
+		if ( strstr ( strEsp8266_Fram_Record .Data_RX_BUF, "+IPD" ) )
+			pRecStr = strEsp8266_Fram_Record .Data_RX_BUF;
+
+	}
+
+	return pRecStr;
+	
+}
+
+/*
+ * 函数名：ESP8266_STA_TCP_Client
+ * 描述  ：WF-ESP8266模块进行STA TCP Clien测试
+ * 输入  ：无
+ * 返回  : 无
+ * 调用  ：被外部调用
+ */
+bool ESP8266_STA_TCP_Client ( void )
+{
+	char *pStr;
+	bool statusCur = false;
+	bool statusLst = true;
+
+	// 液晶初始化
+	LCD_Init();
+	// 设置液晶扫描方向为 左上角->右下角
+	Lcd_GramScan( 1 );
+	LCD_Clear(0, 0, 240, 320, BACKGROUND);
+
+	// 开启sta连接模式
+	LCD_DispStr(0,0,(uint8_t *)"begin to sta connection ...",BLUE);
+	ESP8266_Choose ( ENABLE );	
+	ESP8266_AT_Test ();
+	statusCur = ESP8266_Net_Mode_Choose ( STA );
+	if (statusCur == true)
+	{
+		LCD_DispStr(0,20,(uint8_t *)"sta mode success",0xffff);
+		statusLst = true;
+	}else
+	{
+		LCD_DispStr(0,20,(uint8_t *)"sta mode fail",0xffff);
+		statusLst = false;
+	}
+	
+	//加入热点的SSID和PASSWORD
+	LCD_DispStr(0,40,(uint8_t *)"begin to join ERIC_LAI ...",BLUE);
+	statusCur = ESP8266_JoinAP ( "ERIC_LAI", "lai686868" );
+	if (statusCur == true && statusLst == true)
+	{
+		LCD_DispStr(0,60,(uint8_t *)"join ERIC_LAI success",0xffff);	
+		statusLst = true;
+	} else
+	{
+		LCD_DispStr(0,60,(uint8_t *)"join ERIC_LAI success",0xffff);
+		statusLst = false;
+	}
+
+	// 连接到TCP服务器
+	LCD_DispStr(0,120,(uint8_t *)"begin to connect the server ...",BLUE);
+	statusCur = ESP8266_Link_Server ( enumTCP, serverIpAddress, "9998", Single_ID );
+	if (statusCur == true && statusLst == true)
+	{
+		LCD_DispStr(0,140,(uint8_t *)"connect server success",0xffff);
+		statusLst = true;
+	} else
+	{
+		LCD_DispStr(0,140,(uint8_t *)"connect server fail",0xffff);
+		statusLst = false;
+	}
+	
+	// 开启透传模式下的数据传输
+	LCD_DispStr(0,160,(uint8_t *)"open the send data function ...",BLUE);
+	statusCur = ESP8266_UnvarnishSend();
+	if (statusCur == true && statusLst == true)
+	{
+		LCD_DispStr(0,180,(uint8_t *)"you can send data now",0xffff);
+		// 发送测试数据
+		ESP8266_SendString(ENABLE, "HELLO WORLD\r\n", NULL, Single_ID);
+		//USART_DMACmd(USART2, USART_DMAReq_Rx, ENABLE);
+		pStr = ESP8266_ReceiveString ( ENABLE );
+		PC_Usart("%s", pStr);
+		ESP8266_Return_At();
+		ESP8266_Disable_UnvarnishSend();
+		ESP8266_Tcp_Close();
+		statusLst = true;
+	} else
+	{
+		LCD_DispStr(0,180,(uint8_t *)"send data function fail",0xffff);
+		statusLst = false;
+	}
+	
+	// 设置返回
+	if (statusLst == true) 
+	{
+		return true;
+	}
+	return false;
+}
